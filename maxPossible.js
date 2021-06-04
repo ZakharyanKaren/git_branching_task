c1c908c67e8d7df433aaa61043929780ce0f12ce
Karen Zakharyan <Zakharyan.kar@gmail.com>
2021-06-04 15:39:14 +0400

    feat: add git maxPossible task

---
+++ maxPossible.js
@@ -0,0 +1,16 @@
+function maxPossible(num1, num2, max = null) {
+  arr1 = num1.toString().split("");
+  arr2 = num2.toString().split("");
+
+  const newArr = arr1.map((elem) => {
+    max = null;
+    for (let i = 0; i < arr2.length; i++) {
+      elem < arr2[i] ? (max = arr2[i]) : null;
+    }
+    max !== null ? arr2.splice(arr2.lastIndexOf(max), 1) : null;
+    return max !== null ? max : elem;
+  });
+  return newArr;
+}
+
+console.log(maxPossible(8732, 91255));
